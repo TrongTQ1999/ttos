60463cd5ad60980d7ad0ee8a9afe3c29706fca01
trongtq6 <trongtq1999@gmail.com>
2024-05-07 17:26:10 +0700

    first commit

---
+++ ttos/ttos.c
@@ -0,0 +1,15 @@
+/**
+ ******************************************************************************
+ * @author: TrongTran
+ * @date:   07/05/2024
+ * @brief:  Main defination of active kernel
+ ******************************************************************************
+**/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    
+}
